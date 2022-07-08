7885ea6ade9d45cf389c57da84a1279728a1a1a7
vladliena <sotnikovavladlena@gmail.com>
2022-07-09 00:23:10 +0300

    add new sections

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,24 @@ $(document).ready(function(){
         autoplayHoverPause:true,
         startPosition: 'URLHash'
     });
+    $('.owl-carousel2').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        items:3,
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
 });
 
 $( function() {
